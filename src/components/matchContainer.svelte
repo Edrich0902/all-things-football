b3c16d1a920f8b071b634d47d627c4640e66ff95
Edrich Barnard <edrichbarnard@gmail.com>
2022-11-02 23:33:15 +0200

    feature: add loading animation

--- src/components/matchContainer.svelte
+++ src/components/matchContainer.svelte
@@ -1,5 +1,6 @@
 <script lang='ts'>
 	import type { MatchContainer } from "src/types/propTypes";
+    import { Spinner } from 'flowbite-svelte';
 	import Match from "./match.svelte";
     export let prop: MatchContainer;
 </script>
@@ -8,8 +9,12 @@
 <div class='matchContainer bg-gray-100 dark:bg-gray-700 rounded-lg p-4 mt-8 mb-8 shadow-lg flex flex-col items-center'>
     <span class='text-lg font-medium mb-2'>{prop.title}</span>
     <div class="matches-wrapper w-full flex flex-col justify-center items-center">
-        {#each prop.matches as match}
-            <Match prop={match} />
-        {/each}
+        {#if prop.matches.length > 0}
+            {#each prop.matches as match}
+                <Match prop={match} />
+            {/each}
+        {:else}
+            <Spinner></Spinner>
+        {/if}
     </div>
 </div>

701045afbef345a07435bfb67cd91a0a0bc7ddbd
Edrich Barnard <edrichbarnard@gmail.com>
2022-12-22 16:20:52 +0200

    feature: add initial league layout

--- src/routes/leagues/+page.svelte
+++ src/routes/leagues/+page.svelte
@@ -1,5 +1,27 @@
 <script lang='ts'>
+	import League from "../../components/league.svelte";
+
 
 </script>
 
-<h1>Leagues</h1>
+<svelte:head>
+    <title>All Things Football - Leagues</title>
+</svelte:head>
+
+<div class="leagues-grid gap-x-2 gap-y-2 justify-center">
+    <League />
+    <League />
+    <League />
+    <League />
+    <League />
+    <League />
+    <League />
+    <League />
+</div>
+
+<style>
+    .leagues-grid {
+        display: grid;
+        grid-template-columns: repeat(auto-fit, minmax(400px, 400px));
+    }
+</style>
